6114db0b676e15dda10416422debe0aea162d1a6
almero0922 <albertomedel29@gmail.com>
2025-07-15 11:37:41 -0600

    loopTriangle and fizzbuzz exercises

---
+++ ch2Exercises.js
@@ -0,0 +1,21 @@
+function loopingTriangle(loop){
+    let triangle = "";
+    for(let i = 0; i < loop; i++){
+        console.log(triangle+="#")
+    }
+}
+
+function fizzBuzz(num){
+    for(let i = 1; i <= num; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            console.log("FizzBuzz");
+        }else if(i % 3 == 0){
+            console.log("Fizz");
+        }else if(i % 5 == 0){
+            console.log("Buzz");
+        }else{
+            console.log(i);
+        }
+    }
+
+}
